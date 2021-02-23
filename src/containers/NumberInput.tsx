d26fa2702b801852363a9044cfc362c1099e41f9
Yury Kurkin <kurkin.yura@gmail.com>
2021-02-23 06:06:02 +0100

    design for login form without logic

---
+++ src/containers/NumberInput.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {FieldRenderProps} from 'react-final-form';
+
+type Props = FieldRenderProps<number, any>;
+
+const NumberInput: React.FC<Props> = ({input, meta, ...rest}: Props) => <input {...input} {...rest} type="number" />;
+
+export default NumberInput;
